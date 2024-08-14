2b47576338d63aee7cdaf28d065d41348d364700
Jim Chen <jimchen4214@gmail.com>
2024-08-14 10:53:11 +0800

    Updates

---
+++ vast_ai_gpu_util.sh
@@ -0,0 +1,12 @@
+cd ~
+git clone https://github.com/jimchen2/indoor_localization_oxford_dataset
+python -m venv env
+source env/bin/activate
+cd indoor_localization_oxford_dataset
+pip install -r requirements.txt
+
+
+rm -r data
+wget https://jimchen4214-public.s3.us-east-1.amazonaws.com/other/modified_data.zip
+mv modified_data.zip data.zip
+unzip data.zip
